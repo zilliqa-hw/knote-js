55b96c3875eae4c28580c053e1aab3804196fe6a
Janos Feher <git@janosfeher.com>
2022-08-09 09:13:11 +0100

    fix: switching from SSH-based clone to HTTPS.

--- zarf/terraform/ecr.tf
+++ zarf/terraform/ecr.tf
@@ -6,7 +6,7 @@ module "ecr" {
     aws.root-eu-central-1            = aws.root-eu-central-1
     aws.shared-services-eu-central-1 = aws.shared-services-eu-central-1
   }
-  source = "git@github.com:zilliqa-hw/infra.git//terraform/modules/aws-ecr-apps"
+  source = "git::https://github.com/zilliqa-hw/infra.git//terraform/modules/aws-ecr-apps"
 
   namespace = "thirdparty"
   name      = local.application
